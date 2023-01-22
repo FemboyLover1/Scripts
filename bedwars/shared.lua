e28a40dd1aeaa578f2e8077923e865d515b3076b
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2023-01-21 21:51:49 -0500

    Create shared.lua

---
+++ bedwars/shared.lua
@@ -0,0 +1,17 @@
+local tab = {
+	GameLoaded = function()
+		if game:IsLoaded() then
+			return true
+		end
+		return false
+	end,
+	isAlive = function(plr)
+		if not plr.Character then return false end
+		if not plr.Character.Head then return false end
+		if not plr.Character.PrimaryPart then return false end
+		if plr.Character.Humanoid.Health < 1 then return false end
+		return true
+	end,
+}
+
+shared.Functions = tab
