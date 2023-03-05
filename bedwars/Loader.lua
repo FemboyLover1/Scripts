7292ab45229a671bfdaf8214a8534bf8056147e8
Kitty9 <123281638+Kitty99999@users.noreply.github.com>
2023-03-04 20:39:01 -0500

    Update Loader.lua

--- bedwars/Loader.lua
+++ bedwars/Loader.lua
@@ -1,5 +1,5 @@
-if game.PlaceId == "6872274481" then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitty99999/Scripts/main/bedwars/6872274481.lua", true))()
-else
+if game.PlaceId == "6872265039" then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitty99999/Scripts/main/bedwars/6872265039.lua", true))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitty99999/Scripts/main/bedwars/6872274481.lua", true))()
 end
