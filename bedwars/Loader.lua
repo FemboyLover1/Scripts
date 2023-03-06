89c0b2185c78acb267f6f9044b59fe2d6796e5e3
FemboyLover <123281638+FemboyLover1@users.noreply.github.com>
2023-03-06 18:11:47 -0500

    Update Loader.lua

--- bedwars/Loader.lua
+++ bedwars/Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == "6872265039" then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitty99999/Scripts/main/bedwars/6872265039.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FemboyLover1/Scripts/main/bedwars/6872265039.lua", true))()
 else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitty99999/Scripts/main/bedwars/6872274481.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FemboyLover1/Scripts/main/bedwars/6872274481.lua", true))()
 end
